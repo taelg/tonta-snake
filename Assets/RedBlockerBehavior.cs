2ee279f7c71cb99e46ead826de07d85eda1cc241
Tael Galli <taelgalli@gmail.com>
2025-05-19 16:18:22 -0300

    Add simple animation to red blocker breaking

--- Assets/RedBlockerBehavior.cs
+++ Assets/RedBlockerBehavior.cs
@@ -14,6 +14,29 @@ public class RedBlockerBehavior : MonoBehaviour
 
     public void Break(Action<RedBlockerBehavior> breakCallback)
     {
+        StartCoroutine(AnimateBreaking(breakCallback));
+    }
+
+    private IEnumerator AnimateBreaking(Action<RedBlockerBehavior> breakCallback)
+    {
+        float duration = 0.35f;
+        float elapsedTime = 0f;
+        Color color = sprite.color;
+        Color startColor = new Color(color.r, color.g, color.b, 1);
+        Color endColor = new Color(color.r, color.g, color.b, 0.2f);
+        float startScale = 1f;
+        float endScale = 1.5f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            sprite.color = Color.Lerp(startColor, endColor, elapsedTime / duration);
+            gameObject.transform.localScale = Vector3.Lerp(new Vector3(startScale, startScale, 1), new Vector3(endScale, endScale, 1), elapsedTime / duration);
+            yield return null;
+        }
+
+        sprite.color = color;
+        gameObject.SetActive(false);
         breakCallback?.Invoke(this);
     }
 
